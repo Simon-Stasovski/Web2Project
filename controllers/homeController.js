f1777e4a7002f325c3858053581368d356382264
Simon-Stasovski <simon.sta25@gmail.com>
2022-04-29 15:45:02 -0400

    Basic Skeleton Design for UI and file directory. Use localhost:1339/

---
+++ controllers/homeController.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+const routeRoot = '/';
+
+module.exports = {
+    router,
+    routeRoot,
+    showHome
+}
+/**
+ * Renders the default Home page of the website.
+ * @param {*} request 
+ * @param {*} response 
+ */
+function showHome(request, response) {
+    response.render('home.hbs')
+}
+
+router.get('/', showHome);
