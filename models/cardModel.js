2ccd4d26fe83f7635d6544e4549fa35f3bdc9004
kevinlaskai <60619425+kevinlaskai@users.noreply.github.com>
2022-04-29 16:51:21 -0400

    TODO LIST for Users

---
+++ models/cardModel.js
@@ -0,0 +1,13 @@
+//refractor the User code to include:
+    //Email
+    //Account balance (when creatingthe user)
+    //update for the Account ballance
+    //a bool to see if its private
+    //Update for the account balance(two users, the value, two bools?
+    //reset password update
+    //update for IsPrivate(bool)
+//views
+    //login page
+    //create account page at loginpage
+    //MyAccount page where user can change password basic account info
+    // tabs with all the diffrenct buttons 
