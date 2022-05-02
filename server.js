8f1ae5725aaceef314503bdfce564f121b45cc6d
Anna <78222489+amoscoloni@users.noreply.github.com>
2022-05-02 16:38:42 -0400

    updated files and added centralized database model

--- server.js
+++ server.js
@@ -1,3 +1,16 @@
 const app = require('./app.js');
 const port = 1339;
+const model = require('./models/databaseModel');
 app.listen(port);
+
+let dbName = process.argv[2];
+
+
+if (!dbName) {
+    dbName = 'cardoholics_db';
+} 
+
+model.initialize(dbName, false)
+    .then(
+        app.listen(port) // Run the server
+    );
