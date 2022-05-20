4419af1640e8de5b421dacdb7fb36976d428c278
amoscoloni <78222489+amoscoloni@users.noreply.github.com>
2022-05-20 00:26:02 -0400

    added documentation

--- controllers/cardController.js
+++ controllers/cardController.js
@@ -275,10 +275,11 @@ async function listCardsForSale( request, response ){
 router.get( '/cards/sale', listCardsForSale );
 
 /**
- * 
+ * Applies the filters contained in the request object to the specified array
+ * of card obejects.
  * @param {*} request The object representation of the http request
  * @param {*} cards An array of object representations of cards
- * @returns 
+ * @returns The filtered array of cards.
  */
 function getFilterResults( request, cards ){
     let type = request.query.cardType;
